87df15006e7e376f58c71f8f071926abfe939392
Gaurav153fr <118659860+Gaurav153fr@users.noreply.github.com>
2024-08-25 12:29:07 +0530

    updated featured

--- lib/featured/featuredController.ts
+++ lib/featured/featuredController.ts
@@ -1,4 +1,4 @@
-"use server"
+"use server";
 import featuredModel from "./featuredModel";
 
 async function setFeatured(selected: { id: string; url: string }[]) {
@@ -15,12 +15,19 @@ async function setFeatured(selected: { id: string; url: string }[]) {
   }
 }
 
-async function getFeatured(){
+async function getFeatured() {
+  try {
     const data = await featuredModel.findOne({ type: "carousal" });
-if(data){
-    return data
-}
-else return null
+
+    if (data) {
+      return data;
+    } else {
+      return null;
+    }
+  } catch (err: any) {
+    console.error("Error fetching featured data:", err);
+    return null;
+  }
 }
 
-export{setFeatured,getFeatured}
+export { setFeatured, getFeatured };
