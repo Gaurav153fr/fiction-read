f3dae6208853660c243f3d1b5b308c82dca8d1a6
Gaurav153fr <118659860+Gaurav153fr@users.noreply.github.com>
2024-10-03 18:35:44 +0530

    fixed datase connection

--- lib/database.ts
+++ lib/database.ts
@@ -2,6 +2,12 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
+    // Check if the database is already connected (readyState 1 means connected)
+    if (mongoose.connection.readyState === 1) {
+      console.log("Database is already connected.");
+      return;
+    }
+
     // Listen for connection events only once to avoid potential memory leaks
     mongoose.connection.once("connected", () => {
       console.log("Database connected successfully.");
